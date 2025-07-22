60de1fd1a3e815ed46b29c874ec591a7a5fd76be
Utsav Lamichhane <132714550+utsavlamichhane@users.noreply.github.com>
2025-07-22 10:46:16 -0400

    Update preprocessing.py

--- Microbiome_ASV/Data_Preprocessing/preprocessing.py
+++ Microbiome_ASV/Data_Preprocessing/preprocessing.py
@@ -33,4 +33,44 @@ merged.to_csv('preprocessed_ASV_level_1.csv', index=False)
 print("Saved transposed & merged file as 'preprocessed_ASV_level_1.csv'")
 
 
-##
+##lets look the preprocessed 1
+
+df = preprocessed_1
+
+print("Shape (rows × columns):", df.shape)
+print("\nColumn names:")
+print(df.columns.tolist())
+
+
+print("\nHead (first 5 rows):")
+print(df.head())
+
+
+print("\nTail (last 5 rows):")
+print(df.tail())
+
+
+print("\nInfo:")
+print(df.info())
+
+
+
+####
+
+df = pd.read_csv('preprocessed_ASV_level_1.csv')
+
+
+cols = df.columns.tolist()
+cols.remove('Overall_RFI')
+new_order = cols[:1] + ['Overall_RFI'] + cols[1:]
+
+df = df[new_order]
+
+
+df.to_csv('preprocessed_ASV_level_2.csv', index=False)
+
+print("Saved reordered file as 'preprocessed_ASV_level_2.csv'")
+
+
+
+
